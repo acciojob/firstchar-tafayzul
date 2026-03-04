a3e1110fe05eb04f8cefe38b79d251bfe662fff0
tafayzul <107237505+tafayzul@users.noreply.github.com>
2026-03-04 22:05:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-function firstChar(text) {
-  // your code here
+function firstChar(str) {
+  // Trim leading/trailing spaces
+  const trimmedStr = str.trim();
+  
+  // Return the first character of the trimmed string, 
+  // or an empty string if trimmedStr is empty.
+  return trimmedStr.charAt(0);
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
 //const text = prompt("Enter text:");
-alert(firstChar(text));
+alert(firstChar(str));
